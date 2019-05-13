75a7d55e392cc6922f9c58848c893c2fe39c2625
Anton Sozonov <unboundnomad@gmail.com>
2019-05-13 09:27:31 +0400

    add executorService

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,17 +3,27 @@ import dto.BookingRequest;
 import threads.Consumer;
 import threads.Producer;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.stream.IntStream;
 
 public class Main {
+
+    private static final int PRODUCERS_AMOUNT = 3;
+    private static final int CONSUMERS_AMOUNT = 6;
+    private static final int OPERATIONS_AMOUNT = 15;
 
     public static void main(String[] args) throws InterruptedException {
 
         ThreadsQueue<BookingRequest> requestThreadsQueue = new ThreadsQueue<>(5);
 
-        IntStream.range(0, 3).forEach(i -> new Producer(requestThreadsQueue).start());
+        ExecutorService producerExecutor = Executors.newFixedThreadPool(PRODUCERS_AMOUNT);
+
+        IntStream.range(0, OPERATIONS_AMOUNT).forEach(i -> producerExecutor.submit(new Producer(requestThreadsQueue)) );
         Thread.sleep(1000);
         IntStream.range(0, 6).forEach(i -> new Consumer(requestThreadsQueue).start());
+
+        producerExecutor.shutdown();
 
     }
 }
--- src/main/java/threads/Producer.java
+++ src/main/java/threads/Producer.java
@@ -9,9 +9,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class Producer extends Thread {
 
-    private static final int TOTAL_REQUESTS = 20;
     private static AtomicInteger requestCounter = new AtomicInteger(0);
-
     private Logger logger = Logger.getLogger(Producer.class);
 
     private ThreadsQueue<BookingRequest> queue;
@@ -22,11 +20,9 @@ public class Producer extends Thread {
 
     public void run() {
         try {
-            while (requestCounter.getAndIncrement() < TOTAL_REQUESTS) {
-                BookingRequest bookingRequest = RequestGenerator.generateRequest(requestCounter.get());
-                queue.add(bookingRequest);
-                logger.info("Producer " + this.getName() + " send:      " + bookingRequest.toString());
-            }
+            BookingRequest bookingRequest = RequestGenerator.generateRequest(requestCounter.getAndIncrement());
+            queue.add(bookingRequest);
+            logger.info("Producer " + this.getName() + " send:      " + bookingRequest.toString());
         } catch (Exception e) {
             e.printStackTrace();
         }
